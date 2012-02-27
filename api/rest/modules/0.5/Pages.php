1d466c78ace3c51c3619df62a1f057a2d0b0fb11
aurelien <aurelien@e95e8bb4-7bde-48d2-86d3-6627b9fbe1a0>
2012-02-27 16:15:00 +0000

    Amélioration du web service auquel on peut désormais demander une section avec son titre ou bien un numéro de section

--- api/rest/modules/0.5/Pages.php
+++ api/rest/modules/0.5/Pages.php
@@ -1,17 +1,12 @@
 <?php
 // declare(encoding='UTF-8');
 /**
- * Classe d'exemple de service web du projet eFlore
- * Source des données : {NOM_DU_PROJET} {ADRESSE_WEB_DONNEES_DU_PROJET}
- * Paramètres du service :
- *  - param1 : explication de l'utilisation du param1
- *  - param2 : explication de l'utilisation du param2
- * Exemple :
- * http://localhost/{CODE_DU_PROJET}/services/0.1/Exemple?param1=val1&param2=val2
+ * Web service de consultation d'un page wiki
  *
  * @category	php 5.2
- * @package		lion1906
- * @author		{PRENOM} {NOM}<{PRENOM}@tela-botanica.org>
+ * @package		wapi
+ * @author		Aurélien Peronnet < aurelien@tela-botanica.org>
+ * @author		Jean-Pascal Milcent < jpm@tela-botanica.org>
  * @copyright	Copyright (c) 2011, Tela Botanica (accueil@tela-botanica.org)
  * @license		http://www.cecill.info/licences/Licence_CeCILL_V2-fr.txt Licence CECILL
  * @license		http://www.gnu.org/licenses/gpl.html Licence GNU-GPL
@@ -19,42 +14,149 @@
  */
 class Pages extends Service {
 	
+	private $wiki = null;
 	private $pageNom = null;
-	private $retour = 'html';
+	private $section = null;
+	
+	private $retour = 'txt';
+	private $formats_retour = array('txt','html');
 	
 	public function consulter($ressources, $parametres) {
+		header('Content-type: text/plain');
 		$verifOk = $this->verifierParametres($parametres);
 		if ($verifOk) {
-			// Débuter ici le code du service
 			$this->pageNom = $ressources[0];
-			$wiki = Registre::get('wikiApi');
-			$wiki->setPageCourrante($this->pageNom);
-			$page = $wiki->LoadPage($ressources[0]);
-			if ($this->retour == 'html') {
-				$retour = $wiki->Format($page["body"], "wakka");
-			} else {
-				$retour = $page["body"];
-			}
-			return $retour;
+			$page = $this->consulterPage($ressources[0]);
+			return $this->formaterRetour($page);
 		} else {
-			RestServeur::envoyerEnteteStatutHttp(RestServeur::HTTP_CODE_MAUVAISE_REQUETE);
-			
+			RestServeur::envoyerEnteteStatutHttp(RestServeur::HTTP_CODE_MAUVAISE_REQUETE);		
+		}
+	}
+	
+	private function definirValeurParDefautDesParametres() {
+		if (isset($this->parametres['retour']) == false) {
+			$this->parametres['retour'] = self::MIME_JSON;
+		}
+		if (isset($this->parametres['txt_format']) == false) {
+			$this->parametres['txt_format'] = 'txt';
 		}
 	}
 	
 	private function verifierParametres($parametres) {
 		$ok = true;
-		extract($parametres);
-		if (isset($retour) ) {
-			if (!preg_match('/^(wiki|html)$/', $retour)) {
-				$message = "La valeur du paramètre 'retour' peut seulement prendre les valeurs : wiki et html.";
+		if (isset($parametres['txt_format'])) {
+			if(!in_array($parametres['txt_format'], $this->formats_retour)) {
+				$message = "La valeur du paramètre 'txt.format' peut seulement prendre les valeurs : txt et html.";
 				$this->ajouterMessage($message);
 				$ok = false;
 			} else {
-				$this->retour = $retour;
+				$this->retour = $parametres['txt_format'];
 			}
 		}
+		
+		if(isset($parametres['txt_section_position'])) {
+			$this->section = $parametres['txt_section_position'];
+		}
+		
+		if(isset($parametres['txt_section_titre'])) {
+			$this->section = $parametres['txt_section_titre'];
+		}
+		
 		return $ok;
+	}
+	
+	private function consulterPage($page) {
+		$this->wiki = Registre::get('wikiApi');
+		$this->wiki->setPageCourante($this->pageNom);
+		$page = $this->wiki->LoadPage($page);
+		
+		// attention les wikis sont en ISO !
+		if(Config::get('encodage_appli') != Config::get('encodage_wiki')) {
+			$page["body"] = mb_convert_encoding($page['body'],Config::get('encodage_appli'),Config::get('encodage_wiki'));
+		}
+	
+		if($this->section != null) {
+			$page["body"] = $this->découperPageSection($page["body"], $this->section);
+		}
+	
+		return $page;
+	}
+	
+	private function découperPageSection($contenu_page, $section) {
+	
+		$section_retour = '';
+	
+		if(is_numeric($section)) {
+			$section_retour =  $this->getSectionParNumero($contenu_page, $section);
+		} else {
+			$section_retour =  $this->getSectionParTitre($contenu_page, $section);
+		}
+	
+		return $section_retour;
+	}
+	
+	public function getSectionParNumero($page, $num) {
+		preg_match_all('/(=[=]+[ ]*)(.[.^=]*)+[ ]*=[=]+[.]*/i', $page, $sections, PREG_OFFSET_CAPTURE);
+		$sectionTxt = '';
+		$debut_section = 0;
+		$lg_page = strlen($page);
+		$fin_section = $lg_page;
+		
+		if($num <= count($sections[1]) && $num > 0) {	
+						
+			$debut_section = $sections[1][$num - 1][1];
+			$separateur = trim($sections[1][$num - 1][0]);
+			$separateur_trouve = false;
+						
+			for($i = $num; $i < count($sections[1]); $i++) {
+				$fin_section = $sections[1][$i][1];
+				if($separateur == trim($sections[1][$i][0])) {
+					$separateur_trouve = true;
+					break;
+				}
+			}
+			
+			$fin_section = $separateur_trouve ? $fin_section : $lg_page;
+			$sectionTxt = substr($page, $debut_section, $fin_section - $debut_section);
+		} else {
+			$sectionTxt = '';
+		}	
+
+		return $sectionTxt;
+	}
+	
+	public function getSectionParTitre($page, $titre) {
+		$section = '';
+		$reg_exp = '/((=[=]+)[ ]*'.preg_quote(trim($titre), '/').'[ ]*=[=]+)[.]*/i';
+		$match = preg_split($reg_exp, $page, 2, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+		
+		if(count($match) > 3) {
+			$section = explode(trim($match[2]), $match[3], 2);
+			$section = $match[1].' '.$section[0];
+		} elseif(count($match) == 2) {
+			$section = explode(trim($match[1]), $match[2], 2);
+			$section = $match[0].' '.$section[0];
+		} else {
+			$section = "";
+		}
+		
+		return $section;
+	}
+	
+	private function formaterRetour($page) {
+
+		switch($this->retour) {
+			case 'html':
+				$retour = $this->wiki->Format($page["body"], "wakka");
+				break;
+			default:
+				$retour = $page["body"];
+		}
+		return $retour;
+	}
+	
+	private function formaterRetourHtml($retour) {
+	
 	}
 }	
 ?>
