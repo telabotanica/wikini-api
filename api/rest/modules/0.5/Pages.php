064b6df26042c6473fd75e416646a2d8bef350e6
aurelien <aurelien@e95e8bb4-7bde-48d2-86d3-6627b9fbe1a0>
2013-01-25 11:22:57 +0000

    Correction d'un bug lié au plugin des wikis

--- api/rest/modules/0.5/Pages.php
+++ api/rest/modules/0.5/Pages.php
@@ -111,18 +111,20 @@ class Pages extends Service {
 	}
 	
 	private function consulterPage($page, $section = null) {
-		
 		$this->wiki = Registre::get('wikiApi');
+		// La variable globale wiki est déclarée par les wiki et leurs plugins
+		// un bug lié à certains plugin impose de la redéclarer et la réaffecter
+		global $wiki;
+		$wiki = $this->wiki;
 		$this->wiki->setPageCourante($this->pageNom);
 		$page = $this->wiki->LoadPage($page);
-				
+		
 		if ($page != null) {
 			$page["body"] = $this->convertirTexteWikiVersEncodageAppli($page['body']);
 			if($section != null) {
 				$page["body"] = $this->decouperPageSection($page["body"], $section);
 			}
-		}
-	
+		}	
 		return $page;
 	}
 	
