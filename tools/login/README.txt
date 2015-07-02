2543b6745d421a4ce17efb5620cd495323443365
aurelien <aurelien@e95e8bb4-7bde-48d2-86d3-6627b9fbe1a0>
2015-07-02 16:46:46 +0000

    Ajout d'un tool login utilisable avec le sso tela botanica

---
+++ tools/login/README.txt
@@ -0,0 +1,7 @@
+# Utilisation du sso avec wikini
+
+Afin de pouvoir utiliser le sso, en plus du remplacement de ce tool
+il faut ajouter deux variables de configuration au wiki	:
+
+sso_url => 'https://localhost/annuaire/jrest/auth/'
+use_sso => '1'
---
+++ tools/login/wiki.php
@@ -0,0 +1,13 @@
+<?php
+
+// Partie publique
+
+if (!defined("WIKINI_VERSION"))
+{
+        die ("acc&egrave;s direct interdit");
+}
+
+// Code pour l'inclusion des langues
+include_once 'tools/login/lang/login_'.$wakkaConfig['lang'].'.inc.php';
+
+?>
